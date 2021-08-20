d96684c3627aecdfa3af748a5551ffe5323b033c
longreen-at-home <vl.averkov@mail.ru>
2021-08-20 14:25:02 -0400

    i was merge 2 branch's and delete chenges from other user. recover

--- algorithm/comparison_sort/bubble-sort.c
+++ algorithm/comparison_sort/bubble-sort.c
@@ -1,6 +1,6 @@
 #include "./bubble-sort.h"
 
-int maxEl(int* array, int size)
+static int maxEl(int* array, int size)
 {
 	int res = array[0];
 	size = size-1;
@@ -13,7 +13,7 @@ int maxEl(int* array, int size)
 	return res;
 }
 
-int minEl(int* array, int size)
+static int minEl(int* array, int size)
 {
 	int res = array[0];
 	size = size-1;
@@ -26,7 +26,7 @@ int minEl(int* array, int size)
 	return res;
 }
 
-void swap(void * _a, void * _b)
+static void swap(void * _a, void * _b)
 {
 	int temp = *(int*)_a;
 	*(int*)_a = *(int*)_b;
@@ -35,12 +35,12 @@ void swap(void * _a, void * _b)
 
 void bubble_sort(int* array, int size)
 {
-	while(array[0] != minEl(array, size) && array[size-1] != maxEl(array, size))
+	for(int i = 0; i < size; i += 1)
 	{
-		for(int i = 0; i < size; i += 1)
+		for(int j = 0; j < size; j += 1)
 		{
-			if(array[i] > array[i+1])
-				swap(&array[i], &array[i+1]);
+			if(array[j] > array[j+1])
+				swap(&array[j], &array[j+1]);
 		}
 	}
 }
--- algorithm/comparison_sort/bubble-sort.h
+++ algorithm/comparison_sort/bubble-sort.h
@@ -1,9 +1,6 @@
 #ifndef __BUBBLE_SORT_H__
 #define __BUBBLE_SORT_H__
 
-int maxEl(int* array, int size);
-int minEl(int* array, int size);
-void swap(void* _a, void* _b);
 void bubble_sort(int* array, int size);
 
 #endif
