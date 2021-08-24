96704a0ddba7feaa29b4a32387b883880e45f68a
longreen-at-home <vl.averkov@mail.ru>
2021-08-24 14:13:02 -0400

    fix cl-list

--- struct/list/cl-list.c
+++ struct/list/cl-list.c
@@ -21,18 +21,29 @@ void add_to_CL_DL_List(struct DL_List * dl_list, char * value)
 {
 	struct DL_Node * dl_node = create_DL_Node(value);
 
-	if(dl_list == NULL || dl_list->head == NULL)
+	if(dl_list == NULL)
 	{
+		dl_list = create_CL_DL_List();
 		dl_list->head = dl_node;
 		dl_node->next = dl_node;
 		dl_node->previous = dl_node;
+
+		return;
+	}
+	if(dl_list->head == NULL)
+	{
+		dl_list->head = dl_node;
+		dl_node->next = dl_list->head;
+		dl_node->previous = dl_list->head;
 		
 		return;
 	}
+	
 	dl_node->previous = dl_list->head->previous;
-	dl_node->next = dl_list->head;
 	dl_list->head->previous->next = dl_node;
+	dl_node->next = dl_list->head;
 	dl_list->head->previous = dl_node;
+	dl_list->head = dl_node;
 }
 
 void remove_from_CL_DL_List(struct DL_List * dl_list, char * value)
@@ -50,15 +61,28 @@ void remove_from_CL_DL_List(struct DL_List * dl_list, char * value)
 			{
 				dl_list->head->previous = currentNode->previous;
 				currentNode->previous->next = dl_list->head;
-
+			
 				free(currentNode);
 				currentNode = NULL;
-
+			
 				return;
+			}
+			if(currentNode == dl_list->head)
+			{
+				if(currentNode->previous == dl_list->head)
+				{
+					free(currentNode);
+					currentNode = NULL;
+					dl_list->head = NULL;
+					dl_list = NULL;
+
+					return;
+				}
 			}
 	
 			currentNode->previous->next = currentNode->next;
 			currentNode->next->previous = currentNode->previous;
+			dl_list->head = currentNode->next;
 
 			free(currentNode);
 			currentNode = NULL;
@@ -70,16 +94,33 @@ void remove_from_CL_DL_List(struct DL_List * dl_list, char * value)
 	while(currentNode != dl_list->head);
 }
 
-void print_list(struct DL_List * dl_list)
+void print_CL_List(struct DL_List * dl_list)
 {
 	if(dl_list == NULL || dl_list->head == NULL)
 		return;
 
 	struct DL_Node * currentNode = dl_list->head;
+
 	do
 	{
 		printf("%s", currentNode->value);
+
 		currentNode = currentNode->next;
 	}
 	while(currentNode != dl_list->head);
 }
+
+unsigned int CL_List_size(struct DL_List * dl_list)
+{
+	unsigned int res = 0;
+
+	struct DL_Node * currentNode = dl_list->head;
+	do
+	{
+		currentNode = currentNode->next;
+		res++;
+	}
+	while(currentNode != dl_list->head);
+
+	return res;
+} 
--- struct/list/main.c
+++ struct/list/main.c
@@ -1,21 +1,21 @@
 #include "cl-list.h"
 int main()
-{
+{	
 	struct DL_List * cl_list = create_CL_DL_List();
 
 	add_to_CL_DL_List(cl_list, "value1\n");
 	add_to_CL_DL_List(cl_list, "value2\n");
 	add_to_CL_DL_List(cl_list, "value3\n");
-	add_to_CL_DL_List(cl_list, "value4\n");
 
-	//remove_from_CL_DL_List(cl_list, "value2\n");
-        //remove_from_CL_DL_List(cl_list, "value3\n");
-        remove_from_CL_DL_List(cl_list, "value1\n");
-        //remove_from_CL_DL_List(cl_list, "value4\n");
+	print_CL_List(cl_list);
+	printf("\n\n");
 
-	//print_list(cl_list);
+	remove_from_CL_DL_List(cl_list, "value3\n");
+	remove_from_CL_DL_List(cl_list, "value1\n");
+	remove_from_CL_DL_List(cl_list, "value2\n");
 
-	//free_DL_List(cl_list);
+	print_CL_List(cl_list);
+        printf("\n\n");
 
 	return 0;
 }
